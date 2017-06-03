4d77e6155dabf6c41ffbf9e7d807204cdddc7b4f
Brooks Patton <brooks@brookzerker.com>
2017-06-03 14:55:25 -0600

    added body-parser

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
-
+const bodyParser = require('body-parser')
 const app = express()
 const port = process.env.PORT
 
 app.use(morgan('combined'))
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
 
 app.post('/', (req, res, next) => {
   console.log(req.body)
